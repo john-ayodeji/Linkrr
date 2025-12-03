c7231f416dfbad26efaeff7bce916400939b800c
John Ayodeji <johnayodejie@gmail.com>
2025-12-03 12:26:42 +0100

    Fix syntax error in route registration

--- Routes.go
+++ Routes.go
@@ -14,7 +14,7 @@ func RegisterAuthRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("POST /api/v1/auth/sign-up", authHandler.SignUp)
 	mux.HandleFunc("POST /api/v1/auth/login", authHandler.Login)
 	mux.HandleFunc("GET /api/v1/auth/refresh", authHandler.RenewAccessToken)
-	mux.HandleFunc("GET /api/v1/auth/revoke", authHandler.RevokeRefreshToken)\
+	mux.HandleFunc("GET /api/v1/auth/revoke", authHandler.RevokeRefreshToken)
 	mux.HandleFunc("POST /api/v1/auth/forgot-password", authHandler.ForgotPassword)
 	mux.HandleFunc("POST /api/v1/auth/reset-password", authHandler.ResetPassword)
 }
